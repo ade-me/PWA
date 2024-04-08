2e6014bd70fdda66a838f8bd67af011e02ebc9db
Movicks <codlabx@gmail.com>
2024-04-08 15:58:20 +0100

    setup: initial setup for ready

---
+++ frontend/src/views/homepage/Welcome.jsx
@@ -0,0 +1,11 @@
+function Welcome() {
+
+  return (
+    <div>
+      <h1>Welcome to My Website!</h1>
+      {/* Add any other welcome content here */}
+    </div>
+  );
+}
+
+export default Welcome;
