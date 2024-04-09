9f24b67920c17b9db2d823e87432cbc9cd9c08e5
Movicks <codlabx@gmail.com>
2024-04-09 02:09:01 +0100

    welcompage: just the design without functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './views/homepage/Home'
-import Welcome from './views/homepage/Welcome'
+import Welcome from './views/auths/Welcome'
+import ForgotPassword from './views/auths/ForgotPassword'
+import Register from './views/auths/Register'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
          <Route exact path="/" element={<Welcome />} />
-         <Route exact path="/home" element={<Home />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route path="/register" element={<Register/>} />
+        <Route path='/forgot-password' element={<ForgotPassword/>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/views/auths/Welcome.jsx
@@ -0,0 +1,65 @@
+import LazyLoad from "react-lazy-load";
+import './authsStyle.css'
+import { Link } from "react-router-dom";
+import { AlternateEmail, Apple, FacebookRounded } from "@mui/icons-material";
+
+function Welcome() {
+    
+    // Function to handle sign in with Apple
+    const handleAppleSignIn = () => {
+        console.log("Signing in with Apple...");
+    };
+
+    // Function to handle sign in with Facebook
+    const handleFacebookSignIn = () => {
+        console.log("Signing in with Facebook...");
+    };
+
+    // Function to handle sign in with Gmail
+    const handleGmailSignIn = () => {
+        console.log("Signing in with Gmail...");
+    };
+
+    return (
+        <LazyLoad debounce={500} className="h-screen w-screen bg-black-500">
+            <div className="flex items-center justify-center h-screen bg-white w-screen">
+                <section className="Social-Logins-container">
+                    <div className="Welcome pt-5">
+                        <h1 className="text-black-500 text-center text-3xl md:text-4xl mb-6 font-bold">Welcome</h1>
+                        <p className="text-center text-black-200 text-1xl md:text-2xl mb-5">Sign in to continue</p>
+                        <div className="Socials-box w-full h-auto flex flex-col items-center justify-center mb-8 gap-2">
+                            <div className="Social-btns w-full flex items-center justify-center rounded-full border border-black-500">
+                                <button onClick={handleAppleSignIn} className="w-full text-black-500 flex items-center justify-center gap-4">
+                                    <div className="w-8 h-8 bg-black rounded-full border flex items-center justify-center">
+                                        <Apple className="Social-Icons"/>
+                                    </div> 
+                                    Continue with Apple
+                                </button>
+                            </div>
+                            <div className="Social-btns w-full flex items-center justify-center rounded-full border border-black-500">
+                                <button onClick={handleFacebookSignIn} className="w-full text-black-500 flex items-center justify-center gap-4">
+                                    <div className="w-8 h-8 bg-black rounded-full border flex items-center justify-center">
+                                        <FacebookRounded className="Social-Icons"/>
+                                    </div>
+                                    Continue with Facebook
+                                </button>
+                            </div>
+                            <div className="Social-btns w-full flex items-center justify-center rounded-full border border-black-500">
+                                <button onClick={handleGmailSignIn} className="w-full text-black-500 flex items-center justify-center gap-4">
+                                    <div className="w-8 h-8 bg-black rounded-full border flex items-center justify-center">
+                                        <AlternateEmail className="Social-Icons"/>
+                                    </div>
+                                    Continue with Gmail
+                                </button>
+                            </div>
+                        </div>
+                        <button className="Signin-socials-btn w-full bg-gray-300 mb-6">Sign in</button>
+                        <p className="text-1xl">Do not have an account? &nbsp;<Link to="/register" className="font-bold">Sign Up</Link></p>
+                    </div>
+                </section>
+            </div>
+        </LazyLoad>
+    );
+}
+
+export default Welcome;
